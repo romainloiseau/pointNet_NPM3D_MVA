b378979a7f167301077c7bf94c0e6fb9cc2fbf9e
Romain Loiseau <romain.loiseau@polytechnique.edu>
2019-02-05 10:20:23 +0100

    Update npm3dGenerator.py

--- npm3dGenerator.py
+++ npm3dGenerator.py
@@ -89,8 +89,8 @@ class NPM3DGenerator(keras.utils.Sequence):
         
         self.prepare_NPM3D()
         
-    def get_label(self, data):
-        return = to_categorical(data[:, -1], num_classes = self.n_classes + 1)[:, 1:] if self.train else None
+    def get_label(self, label):
+        return to_categorical(label, num_classes = self.n_classes + 1)[:, 1:] 
         
     def load_point_cloud(self, input_dir):
         data = PlyData.read(input_dir)
@@ -107,7 +107,7 @@ class NPM3DGenerator(keras.utils.Sequence):
                 normal = data[:, 3:6]
             else:
                 normal, eigen = compute_normals(cloud, tree, self.normal_radius, input_dir)
-        label = self.get_label(data)
+        label = self.get_label(data[:, :-1]) if self.train else None
         return cloud, tree, normal, eigen, label
     
     def compute_class_weight(self):
